d50155fdddfdb7fbd20660fa65ff8a221da10ea5
lilkgb <mcleanrs@comcast.net>
2019-01-14 23:08:26 -0800

    removed all changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,3 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
-
-  $("p").click(function() {
-    alert("This is a paragraph.")
-  });
-
-  $("#wizard").click(function() {
-    alert("This is the powerful WIZARD!")
-  });
-
-  $("#btn1").click(function() {
-    $("#btn1").hide("slow");
-  });
-
-  $("h1").click(function() {
-    $("#btn1").show();
-  });
-
-  $("#flip").click(function() {
-    $("#panel").slideDown("slow");
-  });
+  
 });
