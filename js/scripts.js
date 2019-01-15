7e4aa89424217315340e4dabd44b68ec0cb67b8c
lilkgb <mcleanrs@comcast.net>
2019-01-15 07:40:24 -0800

    trying to figure out how to reverse the text

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $("#btn1").click(function() {
-  var response = prompt("Enter a scentence please:");
+  var response = prompt("Enter a scentence please with no puncuation at the end:");
   console.log(response);
+  var first = response.charAt(first);
+  var last = response.charAt(response.length-1);
+  var two = (first + last);
+  alert(two);
+
   });
 });
